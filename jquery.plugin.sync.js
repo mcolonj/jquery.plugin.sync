b9600526691c6009322baf8d2a3002e30b41bf96
Michael Colon <michaelcolon@youaredreaminginspacewheretimeisjustenergy.local>
2016-08-11 00:29:41 -0400

    resync update code improvements

--- jquery.plugin.sync.js
+++ jquery.plugin.sync.js
@@ -1,80 +1,81 @@
 ;(function ( $ ) {
-    $.fn.extend({
-      animateCss: function (animationName, disappear) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        $cached = $(this);
-        $cached.addClass('animated ' + animationName).one(animationEnd, function() {
-            $cached
-              .removeClass('animated')
-              .removeClass(animationName);
-            if (disappear) {
-               console.log('disappearing');
-               $cached.hide();
-             }
-        });
-      }
-    });
 
-    $.fn.sync = function( options ) {
-      var settings = $.extend({
-        frame         : 0,
-        video         : video,
-        runTransition : null,
-        dev           : false,
-      }, options);
-      $video = $( settings.video );
-      // loval variables used in the sync plugin.
+  $.fn.extend({
+    animateCss: function (animationName, disappear) {
+      var animationEnd = 'webkitAnimationEnd mozAniamtionEnd MSAnimationEnd oanimationend animationend';
+      $cached = $(this);
+      $cached.addClass('animated ' + animationName)
+      .one(animationEnd, function() {
+         $cached.removeClass('animated').removeClass(animationName);
+         if (disappear) $cached.hide();
+      });
+    }
+  }); //end fn.extend
+
+
+  $.fn.sync = function (options) {
+
+    var settings = $.extend({
+      elem : this,
+      frame : 0,
+      video : undefined,
+      runTransition : undefined,
+      dev : 0,
+    }, options);
+
+    (undefined === (video = settings.video)) ? function() {
+      console.log('Video property is required');
+    }() : function() {
+      console.log('got here');
+      $video = $(video);
       var transitions = {};
       var outmations = {};
       var frames = [];
       var frame = 0;
       var mytimer = null;
-
-      this.each(function(index, section) {
+      settings.elem.each(function(index, section) {
+        console.log('section');
         $section = $(section);
-        transitions[$section.attr( 'keyframe' )] = {id:$section.attr( 'id' ), animation:$section.attr( 'animation' )};
-        if ($section.attr( 'outframe')) {
-          outmations[$section.attr( 'outframe' )]  = {id:$section.attr( 'id' ), animation:$section.attr( 'outmation' )};
+        transitions[$section.attr('keyframe')] = {id:$section.attr('id'), animation:$section.attr('animation')};
+        if ($section.attr('outframe')) {
+          outmations[$section.attr('outframe')] = {id:$section.attr('id'), animation:$section.attr('outmation')};
         }
       });
 
-      $video.get(0).addEventListener("play", function() {
-          mytimer = setInterval(function(vid, transitions) {
-            if (vid.paused || vid.ended) {
-              return;
-            }
-
-            $counter = $('#counter');
-            if (settings.dev) {
-              $counter.show().html(""+frame);
-            } else {
-              $counter.hide();
-            }
-
-            var key = ""+frame;
-            if (key in transitions) runAnimationForKey(key, transitions, false);
-            if (key in outmations) runAnimationForKey(key, outmations, true);
-            frame++;
-          }, 1000, $video.get(0), transitions);
+      $video.get(0).addEventListener('play', function() {
+        console.log('got play event');
+        mytimer = setInterval(function(){
+          if ($video.get(0).paused || $video.get(0).ended) return;
+          $counter = $('#counter');
+          var key = ""+frame;
+          if (settings.dev) {
+            $counter.show().html(key);
+          } else {
+            $counter.hide();
+          }
+          console.log(key);
+          if (key in transitions) runAnimationForKey(transitions[key], false);
+          if (key in outmations) runAnimationForKey(outmations[key], true);
+          frame++;
+        },1000);
       }, false);
 
-      $video.get(0).addEventListener("ended", function() {
-          clearInterval(mytimer);
-          frame = 0;
+      $video.get(0).addEventListener('ended', function() {
+        clearInterval(mytimer);
+        frame = 0;
       }, false);
 
-      // initial play command sent to the HTML5 Video
-      $video.get(0).play();
-      return this;
-    };
+    }();
 
-    function runAnimationForKey(key, transitions, disappear) {
-      var transition = transitions[key];
-      
-      $.each(transition.animation.split(' '), function(index, animation) {
-        $('#'+transition.id).show();
-        $('#'+transition.id).animateCss(animation, disappear);
-      });
-    };
+    $video.get(0).play();
+    return this;
+  };
 
-}( jQuery ));
+  function runAnimationForKey(transition, disappear) {
+    $.each(transition.animation.split(' '), function(index, animation) {
+      $('#'+transition.id).show();
+      $('#'+transition.id).animateCss(animation, disappear);
+    });
+  }
+
+}(jQuery));
